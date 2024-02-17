e6503b58a8ebe82fcf4094abbde78b74632b37fb
engineer1988 <140731601+engineer1988@users.noreply.github.com>
2024-02-17 14:39:31 +0000

    Deploying to gh-pages from @ engineer1988/goit-js-hw-12@ae7108596853288a790567d77a60c5352eb62082 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,25 +1,25 @@
-import{a as g,i as p,S as h}from"./assets/vendor-427610a5.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function a(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=a(e);fetch(e.href,r)}})();async function f(s,t){const o=`https://pixabay.com/api/?key=42241202-737945e445eb9c5ec6bcca7e8&q=${s}&${t}`;return(await g.get(o)).data}let u;function y(s){return s.hits.length===0&&(u=s.hits.length,p.error({maxWidth:"432px",messageColor:"rgb(250, 250, 251)",messageSize:"16px",backgroundColor:"rgb(239, 64, 64)",position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"})),s.hits.map(a=>`
+import{a as w,i as l,S as $}from"./assets/vendor-427610a5.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const a of s.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function r(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerpolicy&&(s.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?s.credentials="include":t.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(t){if(t.ep)return;t.ep=!0;const s=r(t);fetch(t.href,s)}})();let p;async function f(e,o,r){const i=new URLSearchParams({image_type:"photo",orientation:"horizontal",safesearch:"true",page:o,per_page:r}),s=`https://pixabay.com/api/?key=42241202-737945e445eb9c5ec6bcca7e8&q=${e}&${i}`,a=await w.get(s);return p=a.data.totalHits,a.data}let y;function b(e){return e.hits.length===0&&(y=e.hits.length,l.error({maxWidth:"432px",messageColor:"rgb(250, 250, 251)",messageSize:"16px",backgroundColor:"rgb(239, 64, 64)",position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"})),e.hits.map(r=>`
             <li class="gallery-item">
-            <a class="gallery-link" href="${a.largeImageURL}">
-              <img class="gallery-image" src="${a.webformatURL}" alt="${a.tags}" />
+            <a class="gallery-link" href="${r.largeImageURL}">
+              <img class="gallery-image" src="${r.webformatURL}" alt="${r.tags}" />
             </a>
             <div class="image-text">
              <div class="image-text-item">
               <h2>Likes</h2>
-              <span class="span-size">${a.likes}</span>
+              <span class="span-size">${r.likes}</span>
              </div>
              <div class="image-text-item">
               <h2>Views</h2>
-              <span class="span-size">${a.views}</span>
+              <span class="span-size">${r.views}</span>
              </div>
              <div class="image-text-item">
               <h2>Comments</h2>
-              <span class="span-size">${a.comments}</span>
+              <span class="span-size">${r.comments}</span>
              </div>
              <div class="image-text-item">
               <h2>Downloads</h2>
-              <span class="span-size">${a.downloads}</span>
+              <span class="span-size">${r.downloads}</span>
              </div>
             </div>
-          </li>`).join("")}const c=document.querySelector(".gallery"),d=document.querySelector(".form"),l=document.querySelector(".loader"),n=document.querySelector(".btn"),L={captionSelector:"img",captionsData:"alt",captionDelay:250};let v=1,b=15;const x=new URLSearchParams({image_type:"photo",orientation:"horizontal",safesearch:!0,page:v,per_page:b});let m;l.classList.add("hide");n.classList.add("hide");const S=new h(".gallery a",L);d.addEventListener("submit",s=>{s.preventDefault(),m=s.target.elements.search.value,l.classList.remove("hide"),f(m,x).then(t=>y(t)).then(t=>$(t)).catch(t=>p.error({maxWidth:"432px",messageColor:"rgb(250, 250, 251)",messageSize:"16px",backgroundColor:"rgb(239, 64, 64)",position:"topRight",message:`${t}`})),d.reset()});function $(s){c.innerHTML="",c.innerHTML=s,u===0?n.classList.add("hide"):n.classList.remove("hide"),S.refresh(),l.classList.add("hide")}
+          </li>`).join("")}let n=1,g=15,u;const c=document.querySelector(".gallery"),d=document.querySelector(".form"),L=document.querySelector(".loader"),h=document.querySelector(".btn"),k={captionSelector:"img",captionsData:"alt",captionDelay:250};let m;const z=new $(".gallery a",k);d.addEventListener("submit",e=>{e.preventDefault(),m=e.target.elements.search.value.trim(),v(),n=1,f(m,n,g).then(o=>b(o)).then(o=>x(o)).catch(o=>l.error({maxWidth:"432px",messageColor:"rgb(250, 250, 251)",messageSize:"16px",backgroundColor:"rgb(239, 64, 64)",position:"topRight",message:`${o}`})),d.reset()});h.addEventListener("click",()=>{v(),n+=1,f(m,n,g).then(e=>b(e)).then(e=>x(e)).catch(e=>l.error({maxWidth:"432px",messageColor:"rgb(250, 250, 251)",messageSize:"16px",backgroundColor:"rgb(239, 64, 64)",position:"topRight",message:`${e}`})),d.reset()});function x(e){n===1?(c.innerHTML="",c.innerHTML=e):c.insertAdjacentHTML("beforeend",e),R(),z.refresh(),C(),q()}function v(){L.classList.remove("hidden")}function C(){L.classList.add("hidden")}function P(){h.classList.remove("hidden")}function S(){h.classList.add("hidden")}function R(){y===0?S():P()}function q(){if(u=Math.ceil(p/g),n>=u)return S(),l.error({maxWidth:"432px",messageColor:"rgb(250, 250, 251)",messageSize:"16px",backgroundColor:"rgb(239, 64, 64)",position:"topRight",message:"We're sorry, but you've reached the end of search results."})}
 //# sourceMappingURL=commonHelpers.js.map
